abfcef7c0440ee6a6b4d3fe9e1a89e469c30ec81
Serghei Vlaicu <82963033+vlaicu89@users.noreply.github.com>
2021-06-30 18:48:14 -0500

    Started US100 work3

--- src/test/java/User100.java
+++ src/test/java/User100.java
@@ -2,5 +2,6 @@ public class User100 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Good Bye");
+        System.out.println("Hiiiiiiiiiiiiiiiii");
     }
 }
