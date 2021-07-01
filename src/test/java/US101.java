4b1e62a873cafc7e5a9223f936a5033d182175f7
Serghei Vlaicu <82963033+vlaicu89@users.noreply.github.com>
2021-06-30 19:25:36 -0500

    Started US1o1 has been completed

--- src/test/java/US101.java
+++ src/test/java/US101.java
@@ -1,2 +1,7 @@
 public class US101 {
+    public static void main(String[] args) {
+        System.out.println("awesome work");
+        System.out.println("push to remote");
+        System.out.println("merge into master");
+    }
 }
